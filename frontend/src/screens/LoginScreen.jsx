c69f2719e78db326ca6405b4988d26c3b6af3189
Benjamin-Ettey <benjaminettey2005@gmail.com>
2026-03-18 22:06:55 +0300

    Feature: added frontend components

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,11 +1,29 @@
 import { Link, useNavigate } from "react-router-dom";
 import AuthLayout from "../components/auth/AuthLayout";
+import {useState} from "react";
 
 export default function LoginScreen() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     const navigate = useNavigate();
 
-    const handleLogin = () => {
-        localStorage.setItem("token", "demo-token");
+    const handleLogin = async () => {
+        const response = await fetch("http://localhost:5000/api/auth/login", {
+            method: "POST",
+            headers: { "content-type": "application/json"},
+            body: JSON.stringify({email, password})
+        });
+
+        const data = await response.json();
+
+        if (response.ok){
+            localStorage.setItem("token", data.token);
+            console.log("Login successful");
+        }
+        else{
+            console.log("Login failed");
+        }
         navigate("/chat", { replace: true });
     };
 
@@ -26,12 +44,16 @@ export default function LoginScreen() {
                 <input
                     placeholder="Email"
                     className="w-full mb-3 px-3 py-2 rounded-lg bg-midnight border border-midnight-border text-white outline-none"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                 />
 
                 <input
                     placeholder="Password"
                     type="password"
                     className="w-full mb-4 px-3 py-2 rounded-lg bg-midnight border border-midnight-border text-white outline-none"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                 />
         </AuthLayout>
     );
--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -4,36 +4,54 @@ import AuthLayout from "../components/auth/AuthLayout";
 
 export default function RegisterScreen() {
     const navigate = useNavigate();
-    const [formData, setFormData] = useState({
-        fullName: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-    });
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    // const [formData, setFormData] = useState({
+    //     fullName: "",
+    //     email: "",
+    //     password: "",
+    //     confirmPassword: "",
+    // });
     const [error, setError] = useState("");
 
-    const handleChange = ({ target: { name, value } }) => {
-        setFormData((prev) => ({
-            ...prev,
-            [name]: value,
-        }));
-    };
+    // const handleChange = ({ target: { name, value } }) => {
+    //     setFormData((prev) => ({
+    //         ...prev,
+    //         [name]: value,
+    //     }));
+    // };
 
-    const handleRegister = (event) => {
+    const handleRegister = async (event) => {
         event.preventDefault();
 
-        if (!formData.fullName || !formData.email || !formData.password) {
+        const response = await fetch("http://localhost:5000/api/auth/register", {
+            method: "POST",
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify({username, email, password}),
+        })
+
+        const data = await response.json();
+
+        if (response.ok){
+            console.log("Register successful", data);
+        }
+        else{
+            console.log("Registering failed");
+        }
+        if (!username || !email || !password) {
             setError("Please complete all fields.");
             return;
         }
 
-        if (formData.password !== formData.confirmPassword) {
+        if (password !== confirmPassword) {
             setError("Passwords do not match.");
             return;
         }
 
-        localStorage.setItem("token", "demo-token");
-        localStorage.setItem("user", formData.fullName);
         navigate("/chat", { replace: true });
     };
 
@@ -54,8 +72,8 @@ export default function RegisterScreen() {
         >
                     <input
                         name="fullName"
-                        value={formData.fullName}
-                        onChange={handleChange}
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
                         placeholder="Full name"
                         className="w-full mb-3 px-3 py-2 rounded-lg bg-midnight border border-midnight-border text-white outline-none"
                     />
@@ -63,8 +81,8 @@ export default function RegisterScreen() {
                     <input
                         name="email"
                         type="email"
-                        value={formData.email}
-                        onChange={handleChange}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                         placeholder="Email"
                         className="w-full mb-3 px-3 py-2 rounded-lg bg-midnight border border-midnight-border text-white outline-none"
                     />
@@ -72,8 +90,8 @@ export default function RegisterScreen() {
                     <input
                         name="password"
                         type="password"
-                        value={formData.password}
-                        onChange={handleChange}
+                        value={password}
+                        onChange={(e)=>setPassword(e.target.value)}
                         placeholder="Password"
                         className="w-full mb-3 px-3 py-2 rounded-lg bg-midnight border border-midnight-border text-white outline-none"
                     />
@@ -81,8 +99,8 @@ export default function RegisterScreen() {
                     <input
                         name="confirmPassword"
                         type="password"
-                        value={formData.confirmPassword}
-                        onChange={handleChange}
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
                         placeholder="Confirm password"
                         className="w-full mb-4 px-3 py-2 rounded-lg bg-midnight border border-midnight-border text-white outline-none"
                     />
